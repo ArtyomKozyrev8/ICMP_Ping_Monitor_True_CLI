6dc9d58c351896f10d93ecff098162a306e14cc6
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2019-02-06 10:28:41 +0300

    Update iplist_file_op.py

--- iplist_file_op.py
+++ iplist_file_op.py
@@ -1,4 +1,4 @@
-def write_ip_to_file(ip: str, filename: str = "IPLIST.txt") -> None:
+def write_ip_to_file(ip: str, filename: str = "IPLIST.py") -> None:
     try:
         with open(filename, mode='a') as f:
             f.write(f"{ip}\n")
@@ -6,15 +6,15 @@ def write_ip_to_file(ip: str, filename: str = "IPLIST.txt") -> None:
         print(f"The following unexpected event happened {ex}")
 
 
-def read_ip_from_file(filename: str = "IPLIST.txt") -> tuple:
+def read_ip_from_file(filename: str = "IPLIST.py") -> tuple:
     ip_list = []
     try:
         with open(filename, mode="r") as f:
             for ip in f:
                 ip_list.append(ip.strip())
     except FileNotFoundError:
-        print("The file IPLIST.txt is not reachable or corrupted.")
-        print("Try to delete IPLIST.txt if it exists.")
+        print("The file IPLIST.py is not reachable or corrupted.")
+        print("Try to delete IPLIST.py if it exists.")
         print("Import failed.\n")
         return False, set(ip_list)
     else:
@@ -24,20 +24,20 @@ def read_ip_from_file(filename: str = "IPLIST.txt") -> tuple:
 def remove_ip_from_file(ip: str, ip_list: list) -> None:
     if ip in ip_list:
         ip_list.remove(ip)
-        print(f"The {ip} was removed from ip list file IPLIST.txt")
+        print(f"The {ip} was removed from ip list file IPLIST.py")
     else:
-        print(f"The {ip} is not in ip list file IPLIST.txt")
+        print(f"The {ip} is not in ip list file IPLIST.py")
 
 
-def rewrite_file(iplist:list, file: str = "IPLIST.txt") -> None:
+def rewrite_file(iplist:list, file: str = "IPLIST.py") -> None:
     with open(file, mode='w') as f:
         for ip in set(iplist):
             f.write(f"{ip}\n")
 
 
 def show_ip_in_file(ip_list: list):
-    '''The function used to show ip addressed, which are in unique file IPLIST.txt'''
-    print("The following ips are in file IPLIST.txt:\n")
+    '''The function used to show ip addressed, which are in unique file IPLIST.py'''
+    print("The following ips are in file IPLIST.py:\n")
     for ip in set(ip_list):
         print(ip)
     print()

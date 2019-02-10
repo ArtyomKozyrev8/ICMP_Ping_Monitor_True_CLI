7e8ef46402057239f1c7be27375e18cbab5c2683
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2019-02-10 11:40:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import cli_menu
-
+import time
 from cli_menu_wrap_lib import menu_wrapper
 
 def main():
-    popenList = {} # list of popen subprocess
+    popen_list = {} # dictionary of popen subprocesses
     cli_menu.hello_banner()
     while True:
         command = input("CLI>: ")
         command = cli_menu.analyze_command(command)
         if command[0] in menu_wrapper.keys():
-            menu_wrapper[command[0]](popenList, command)
+            menu_wrapper[command[0]](popen_list, command)
         elif command[0] == "FreeSpace":
             print()
         else:
@@ -17,5 +17,8 @@ def main():
 
 
 if __name__ == '__main__':
+    #try:
     main()
-
+    #except Exception as ex:
+        #print(f"The following error took place {ex}")
+        #time.sleep(30)

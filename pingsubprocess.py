f234aa47447d957875d9671d4754b22291e9762a
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2019-02-12 09:57:11 +0300

    Update pingsubprocess.py

--- pingsubprocess.py
+++ pingsubprocess.py
@@ -36,8 +36,8 @@ def upload_smtp_settings():
     except FileNotFoundError:
         sys.stderr.write("Settings file does not exist or corrupted.\n\n")
         sys.stderr.write("Delete the file if it exists and do setup command\n\n")
-        sys.stderr.write(f"{ip} session crushed.")
-        sys.stderr.write("To restore session to the ip, remove it from monitoring, then add it again!")
+        sys.stderr.write(f"{ip} session crushed.\n\n")
+        sys.stderr.write("To restore session to the ip, add it again!\n")
         sys.stderr.flush()
         sys.exit()
     return settings
@@ -53,7 +53,7 @@ def upload_recipients_list():
         sys.stderr.write("email_recipient_list.py file does not exist or corrupted.\n\n")
         sys.stderr.write("Delete the file if it exists and do recipients command\n\n")
         sys.stderr.write(f"{ip} session crushed.\n\n")
-        sys.stderr.write("To restore session to the ip, remove it from monitoring, then add it again!")
+        sys.stderr.write("To restore session to the ip, add it again!\n")
         sys.stderr.flush()
         sys.exit()
     return recipients
@@ -61,12 +61,15 @@ def upload_recipients_list():
 
 def notificator(pingFailedLetterWasSent, negativePingsInRow, positivePingsInRow,
                 email_sender_box, email_recepient_list, error_mail_message, recovery_mail_message,
-                email_sender_password, smtp_settings, smtp_port):
+                email_sender_password, smtp_settings, smtp_port, mode):
 
     if negativePingsInRow == 4 and not pingFailedLetterWasSent:
         pingFailedLetterWasSent = True
         sys.stderr.write(f"{ip} is not reachable.\n\n")
         sys.stderr.flush()
+
+        if mode == "short":
+            ping_op.write_ping_result_to_file_short_version(pingFailedLetterWasSent, ip)
 
         negativeLetterThread = threading.Thread(target=mail_activity.send_negative_mail,
                                                 args=(f"{ip}", email_sender_box,
@@ -84,6 +87,9 @@ def notificator(pingFailedLetterWasSent, negativePingsInRow, positivePingsInRow,
         sys.stderr.write(f"{ip} is  reachable again now.\n\n")
         sys.stderr.flush()
 
+        if mode == "short":
+            ping_op.write_ping_result_to_file_short_version(pingFailedLetterWasSent, ip)
+
         positiveLetterThread = threading.Thread(target=mail_activity.send_positive_mail,
                                                 args=(f"{ip}", email_sender_box,
                                                       email_recepient_list,
@@ -96,12 +102,12 @@ def notificator(pingFailedLetterWasSent, negativePingsInRow, positivePingsInRow,
         positiveLetterThread.join()
     return pingFailedLetterWasSent
 
-def main(ip):
 
+def main(ip):
     error_mail_message = upload_error_notification_msg()
     recovery_mail_message = upload_recovery_notification_msg()
     if error_mail_message is None or recovery_mail_message is None:
-        sys.stderr.write(f"{ip} session crushed.\n\n")
+        sys.stderr.write(f"{ip} session crushed.\n")
         sys.stderr.flush()
         sys.exit()
 
@@ -113,29 +119,32 @@ def main(ip):
     email_sender_password = settings[1]
     smtp_settings = settings[2]
     smtp_port = settings[3]
+    log_mode = settings[4]
 
     pingFailedLetterWasSent = False
-    positivePingsThisHourCounterC = 0
-    negativePingsThisHourCounterC = 0
     positivePingsInRow = 0
     negativePingsInRow = 0
-    lastAttemptTime = MyTime()
-    previousFilePath = ping_op.write_ping_result_to_file(ip=ip, pingresult=None)
+    if log_mode == "long":
+        positivePingsThisHourCounterC = 0
+        negativePingsThisHourCounterC = 0
+        lastAttemptTime = MyTime()
+        previousFilePath = ping_op.write_ping_result_to_file(ip=ip, pingresult=None)
     while (True):
         pingResult = ping_op.ping(ip)
-        CurrentFilePath = ping_op.write_ping_result_to_file(pingResult, ip)
-        currentTime = MyTime()
+        if log_mode == "long":
+            CurrentFilePath = ping_op.write_ping_result_to_file(pingResult, ip)
+            currentTime = MyTime()
 
-        if currentTime.compare_dates(lastAttemptTime):
-            ping_op.write_ping_stats_to_file(ip, positivePingsThisHourCounterC,
-                                             negativePingsThisHourCounterC, previousFilePath)
-            previousFilePath = CurrentFilePath
-            lastAttemptTime = currentTime
-            positivePingsThisHourCounterC = 0
-            negativePingsThisHourCounterC = 0
+            if currentTime.compare_dates(lastAttemptTime):
+                ping_op.write_ping_stats_to_file(ip, positivePingsThisHourCounterC,
+                                                 negativePingsThisHourCounterC, previousFilePath)
+                previousFilePath = CurrentFilePath
+                lastAttemptTime = currentTime
+                positivePingsThisHourCounterC = 0
+                negativePingsThisHourCounterC = 0
 
-        positivePingsThisHourCounterC = positivePingsThisHourCounterC + pingResult[0]
-        negativePingsThisHourCounterC = negativePingsThisHourCounterC + pingResult[1]
+            positivePingsThisHourCounterC = positivePingsThisHourCounterC + pingResult[0]
+            negativePingsThisHourCounterC = negativePingsThisHourCounterC + pingResult[1]
 
         if positivePingsInRow < positivePingsInRow + pingResult[0]:
             positivePingsInRow = positivePingsInRow + pingResult[0]
@@ -146,10 +155,10 @@ def main(ip):
 
         pingFailedLetterWasSent = notificator(pingFailedLetterWasSent, negativePingsInRow, positivePingsInRow,
                                               email_sender_box, email_recepient_list, error_mail_message,
-                                              recovery_mail_message, email_sender_password, smtp_settings, smtp_port)
+                                              recovery_mail_message, email_sender_password, smtp_settings,
+                                                   smtp_port, log_mode)
 
 
 if __name__ == '__main__':
     ip = str(sys.argv[1])
     main(ip)
-

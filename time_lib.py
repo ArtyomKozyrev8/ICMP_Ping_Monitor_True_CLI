a56a3861ccca4484948b53b2258d797e796e4097
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2019-02-12 09:57:44 +0300

    Update time_lib.py

--- time_lib.py
+++ time_lib.py
@@ -7,7 +7,7 @@ class MyTimeMode(enum.Enum):
      to determine options of __str__ format for that class'''
     full = "Date: {2}.{1}.{0} Time UTC+3 {3}:{4}:{5}"
     middle = "date{2}_{1}_{0}_timeUTC3_{3}"
-    short = "date{2}_{1}_{0}"
+    short = "date{2}_{1}_{0}" # year
 
 
 class MyTime:

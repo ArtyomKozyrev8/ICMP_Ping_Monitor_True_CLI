d48361b58ab6d2092296ac5a3109c23ba5b6cfb4
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2019-02-18 17:17:36 +0300

    Update cli_menu.py

--- myscripts/cli_menu.py
+++ myscripts/cli_menu.py
@@ -2,11 +2,8 @@ import subprocess
 import ipaddress
 import time
 import sys
-import re
 import os
-
-from myscripts import iplist_file_op
-
+from myscripts import database_op
 
 def give_help_menu():
     print()
@@ -17,7 +14,8 @@ def give_help_menu():
     print("        IF YOU USE THE PROGRAM FIRST TIME DO recipients command!\n")
     print("add:    Print add and press Enter to add ip to monitoring.")
     print("        Pings are made within certain interval.")
-    print("        Example: add 1.1.1.1 10")
+    print("        Example: add 1.1.1.1 10 hostname")
+    print("        Example: add 1.1.1.1 10 ")
     print("        Example: add address interval_in_seconds\n")
     print("del:    Print del and press Enter to remove ip from monitoring.")
     print("        Example: del 1.1.1.1 \n")
@@ -46,7 +44,7 @@ def is_ip_address(ip: str) -> bool:
     try:
         ipaddress.IPv4Address(address=str(ip))
     except ipaddress.AddressValueError:
-        print(f"IP {ip} is an incorrect address. It can't be added to monitoring.\n")
+        print("IP {} is an incorrect address. It can't be added to monitoring.\n".format(ip))
         return False
     else:
         return True
@@ -56,7 +54,7 @@ def is_digit(interval: str) -> bool:
     try:
         int(interval)
     except ValueError:
-        print(f"IP {interval} is not correct time interval in seconds.\nIt can't be added to monitoring.\n")
+        print("IP {} is not correct time interval in seconds.\nIt can't be added to monitoring.\n".format(interval))
         return False
     else:
         return True
@@ -76,7 +74,7 @@ def analyze_command(command: str) -> list:
         if element == "":
             copied_command.remove(element)
     command = copied_command
-    if len(command) > 3 or len(command) == 0: # seconds part!!!!
+    if len(command) > 4 or len(command) == 0: # seconds part!!!!
         command = ["none"]
     return command
 
@@ -95,19 +93,17 @@ def exit_program(ip_in_monitoring_dict: dict) -> None:
 
 def show_ip_in_monitoring(ip_in_monitoring_dict: dict) -> None:
     if not len(ip_in_monitoring_dict) == 0:
-        ip_list_in_file = iplist_file_op.read_ip_from_file()[1]
+        ip_list_in_db = database_op.extract_ips_from_ipsessions_table()
         for ip in ip_in_monitoring_dict.keys():
-            for element in ip_list_in_file:
-                if re.search(string=element, pattern=ip) is not None:
-                    print(" ".join(element.split("INTERVAL")))
-                    break
+            address, interval, host = database_op.extract_parameters_of_ip_session_ipsessions_table(ip)
+            print("{} {} {}".format(address, interval, host))
     else:
         print("There is no ip in monitoring now.\n")
 
 
 def is_ip_already_in_monitoring(ip: str, ip_in_monitoring_dict: dict) -> bool:
     if ip in ip_in_monitoring_dict.keys():
-        print(f"{ip} address is already in monitoring.\n")
+        print("{} address is already in monitoring.\n".format(ip))
         return True
     else:
         return False
@@ -118,14 +114,16 @@ def remove_ip_from_monitoring(ip: str, ip_in_monitoring_dict: dict) -> None:
     if ip in ip_in_monitoring_dict.keys():
         ip_in_monitoring_dict[ip].kill()
         del ip_in_monitoring_dict[ip]
-        print(f"{ip} is removed from monitoring\n")
+        print("{} is removed from monitoring\n".format(ip))
     else:
-        print(f"The mentioned ip {ip} address is not in monitoring.\n")
+        print("The mentioned ip {} address is not in monitoring.\n".format(ip))
         print("Use show command to see all ip which are monitored.")
         print("To get help print help then press Enter\n")
 
 
-def add_ip_to_monitoring(ip: str, interval: int, ip_in_monitoring_dict: dict) -> None:
+def add_ip_to_monitoring(session, ip_in_monitoring_dict: dict) -> None:
+    ip = session.ip
+    interval = session.interval
     if is_ip_address(ip):
         if not is_ip_already_in_monitoring(ip, ip_in_monitoring_dict):
             if sys.platform == 'win32':
@@ -135,32 +133,40 @@ def add_ip_to_monitoring(ip: str, interval: int, ip_in_monitoring_dict: dict) ->
                 ip_in_monitoring_dict[ip] = subprocess.Popen(["python3", "pingsubprocess.py", ip, interval],
                                                              stdout=subprocess.DEVNULL)
             else:
-                print(f"The program is not designed to work in your OS {sys.platform}")
+                print("The program is not designed to work in your OS {}".format(sys.platform))
                 print("The program will be terminated in 5 seconds, Sorry...")
                 time.sleep(5)
                 print("Bye")
                 time.sleep(1)
                 sys.exit()
 
-            print(f"{ip} was added to monitoring\n")
-            iplist_file_op.write_ip_to_file(f"{ip}INTERVAL{interval}")  #!!!!
+            print("{} was added to monitoring".format(ip))
+            database_op.insert_line_into_ipsessions_table(session)
 
 
-def import_ip_from_file() -> set:
-    read_from_file_result = iplist_file_op.read_ip_from_file()
-    continue_import = read_from_file_result[0]
-    ip_list = read_from_file_result[1]
-    if not continue_import:
-        ip_list = "FileError"
+def import_ip_from_file():
+    way = os.path.join(os.getcwd(), "database")
+    if not os.path.exists(way):
+        os.makedirs(way)
+    dbname = os.path.join(way, 'ipsessions.db')
+    do_database_exist = os.path.exists(dbname)
+    if do_database_exist:
+        continue_import = True
+    else:
+        print("The database was not created yet")
+        ip_list = []
         return ip_list
     while continue_import:
-        iplist_file_op.show_ip_in_file(ip_list)
+        ip_list = database_op.show_lines_into_ipsessions_table()
         ip = input("Print ip you would like to remove, otherwise print Enter:\n")
         if ip == "":
             break
         else:
-            ip_list = iplist_file_op.remove_ip_from_file(ip, ip_list)
-            iplist_file_op.show_ip_in_file(ip_list)
+            if ip in ip_list:
+                database_op.del_line_from_ipsessions_table(ip, ip_list)
+                ip_list.remove(ip)
+            else:
+                print("Incorrect ip was chosen was made, please try again.\n")
         while True:
             remove_more_ip = input("Print yes and press Enter to remove more ip, otherwise press Enter:\n")
             if remove_more_ip.upper() == "YES":
@@ -168,7 +174,6 @@ def import_ip_from_file() -> set:
                 break
             elif remove_more_ip == "":
                 continue_import = False
-                iplist_file_op.rewrite_file(ip_list)
                 break
             else:
                 print("Incorrect input was made, please try again.\n")
@@ -219,16 +224,16 @@ def setup():
 
     settings = (sender_email, email_sender_password, smtp_settings, str(smtp_port), log_mode)
     print("You made the following settings:")
-    print(f" sender email: {sender_email}\n", f"sender email password: {email_sender_password}\n",
-          f"SMTP server address: {smtp_settings}\n", f"SMTP port number: {smtp_port}\n",
-          f"Log Mode: {log_mode}\n")
+    print(" sender email: {}\n".format(sender_email), "sender email password: {}\n".format(email_sender_password),
+          "SMTP server address: {}\n".format(smtp_settings), "SMTP port number: {}\n".format(smtp_port),
+          "Log Mode: {}\n".format(log_mode))
     print("If you made something wrong, repeat setup!")
     way = os.path.join(os.getcwd(), "settings")
     if not os.path.exists(way):
         os.makedirs(way)
     with open(file=os.path.join(way, "settings.py"), mode="w") as f:
         for i in settings:
-            f.write(f"{i}\n")
+            f.write("{}\n".format(i))
 
 
 def make_email_recipient_list():
@@ -251,17 +256,10 @@ def make_email_recipient_list():
                     print("Incorrect input, please try again.")
         else:
             print("You put incorrect email, please try again!")
-    print(f"As a result you have the following list of recipients:\n{email_recipients}")
+    print("As a result you have the following list of recipients:\n{}".format(email_recipients))
     way = os.path.join(os.getcwd(), "settings")
     if not os.path.exists(way):
         os.makedirs(way)
     with open(file=os.path.join(way, "email_recipient_list.py"), mode="w") as f:
         for i in email_recipients:
-            f.write(f"{i}\n")
-
-
-
-
-
-
-
+            f.write("{}\n".format(i))
